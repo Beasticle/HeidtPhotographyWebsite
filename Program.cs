3bbfd148742f632039a001b1efc52ce85d7c5e69
beasticle <julian.heidt@gmail.com>
2023-06-05 15:08:23 -0400

    first working base

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["PhotoStorageConnection:blob"], preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["PhotoStorageConnection:queue"], preferMsi: true);
+});
 
 var app = builder.Build();
 
